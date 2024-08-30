70c87fd3c67017092eaf939dd930fa14be47fa6c
Wabwizi <josephdanson97@gmail.com>
2024-08-30 21:05:18 +0300

    Create user_python.py

---
+++ user_python.py
@@ -0,0 +1,11 @@
+# Get user's name
+name = "Jose"
+
+# Get user's age
+age = 20
+
+# Get user's location
+location = "Kenya"
+
+# Print personalized message
+print(f"Hello {name}, you are {age} years old and live in {location}.")
