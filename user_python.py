f78b2c4be94a2ede6922dd7028cc262869399fda
Wabwizi <josephdanson97@gmail.com>
2024-08-30 21:08:08 +0300

    Add files via upload

--- user_python.py
+++ user_python.py
@@ -1,11 +1,11 @@
 # Get user's name
-name = "Jose"
+name = input("Hello! What's your name? ")
 
-# Get user's age
-age = 20
+# Get user's age (convert input to integer)
+age = int(input("How old are you, " + name + "? "))
 
 # Get user's location
-location = "Kenya"
+location = input("Where do you live, " + name + "? ")
 
 # Print personalized message
 print(f"Hello {name}, you are {age} years old and live in {location}.")
